1ff033649490cbc45c68368f7c8d6179cf3186fe
Lavi <lavisharma796@gmail.com>
2019-01-11 15:59:45 +0000

    Updates

---
+++ precache-manifest.58a2d6bf54306cec7cd71246c6bc9e51.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fa0694e2df8b00fcafc",
+    "url": "/Wikipedia-Search/static/js/runtime~main.7fa0694e.js"
+  },
+  {
+    "revision": "883a3cd43827ad305882",
+    "url": "/Wikipedia-Search/static/js/main.883a3cd4.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/Wikipedia-Search/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "80269674b8d8299f9fe600d383550d0f",
+    "url": "/Wikipedia-Search/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Wikipedia-Search/precache-manifest.58a2d6bf54306cec7cd71246c6bc9e51.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Wikipedia-Search/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
